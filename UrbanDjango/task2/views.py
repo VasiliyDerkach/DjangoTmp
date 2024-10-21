f9bccd0222a3c8c8ff29ca56fcb87cca49654b41
1 <v_derkach@inbox.ru>
2024-10-21 13:33:56 +0500

    2110+

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def class_tmpl(request):
+    return render(request,"class_template.html")
+def func_tmpl(request):
+    return render(request,"func_template.html")
