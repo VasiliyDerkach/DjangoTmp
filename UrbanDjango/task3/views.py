a401e7ea50c3188c46f233a3cefe22a1f600cc1d
1 <v_derkach@inbox.ru>
2024-10-24 14:45:54 +0500

    2410

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 def main(request):
     return render(request,"third_task/mainpage.html")
 def price(request):
-    return render(request,"third_task/price.html")
+    context = {'1': 'Atomic Heart', '2': "Cyberpunk 2077", '3': 'Monopolya' }
+    return render(request,"third_task/price.html", context)
 def card(request):
     return render(request,"third_task/card.html")
 
